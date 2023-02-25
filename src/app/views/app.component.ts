fddfd2ced24ec52320ebc576b499137708c15915
Denis Ibanez <denis.ibanez@outlook.com>
2023-02-25 17:47:33 -0300

    feature: update angular 14 and install material

--- src/app/views/app.component.ts
+++ src/app/views/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demarco';
+  title = 'Demarco';
+  value = 'Clear me';
 }
